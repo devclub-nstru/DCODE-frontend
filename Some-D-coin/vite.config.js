aaddd217b2a9074c8f7bc77c352c57fc76cc976e
Raghavg18 <graghav2006@gmail.com>
2025-03-12 00:14:18 +0530

    Update vite.config.js

--- Some-D-coin/vite.config.js
+++ Some-D-coin/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['dcode-frontend.onrender.com'],
   }
 })
