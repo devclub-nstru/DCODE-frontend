6ec72165f9d0d89d2b1c8d65ea1bfe8ac66e393d
Raghavg18 <graghav2006@gmail.com>
2025-03-18 11:41:21 +0530

    init

--- Some-D-coin/src/components/About/About.jsx
+++ Some-D-coin/src/components/About/About.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import "./About.css"
+import React from 'react';
+import "./About.css";
 
 function About() {
   return (
-    <div className='main-about'>About</div>
-  )
+    <div className='main-about'>
+      <h1>What is Open-Source?</h1>
+      <p>
+        Open-source means that the code is publicly available for anyone to view, use, and modify. Think of it like a recipe that anyone can improve, customize, or share.
+      </p>
+      <p>
+        By contributing to open-source projects, developers can fix bugs, add features, and help make the software better for everyone. It’s all about collaboration, learning, and building together!
+      </p>
+      
+      <h1>What is DCODE?</h1>
+      <p>
+        DCODE is an event where developers come together to work on open-source projects. At DCODE, participants pick problems (called “issues”) from a list of open-source projects they want to work on. They then “buy” these issues using D-Coins, meaning they commit to solving them. 
+      </p>
+      <p>
+        Once the issues are fixed, they help improve the project, and everyone benefits because the code becomes better. If the participants find an issue or bug while working on the project, they can raise a petition. If the issue is valid, it can be listed on the shop for others to buy in case the person who raised the issue is willing to work on that particular issue.
+      </p>
+      <p>
+        It’s like a hackathon, but instead of just building new things from scratch, you’re helping improve existing projects that anyone can use. It’s all about teamwork, learning, and contributing to the wider tech community.
+      </p>
+      
+      <h1>What are D-Coins?</h1>
+      <p>
+        D-Coins are like a digital currency that will be used in buying the issues and other exciting rewards up for grabs. It will also serve as a point system for the leaderboard throughout the whole event.
+      </p>
+      <p>
+        Every participant will get their own wallets, which will help them store these coins. They can also transfer these coins among themselves if they are willing to do so.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
