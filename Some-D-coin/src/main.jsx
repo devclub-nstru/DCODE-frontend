a37b5a1f54be5cfa28b3f2f1581f8ba02beb3c19
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-03-20 01:31:09 +0530

    Update main.jsx

--- Some-D-coin/src/main.jsx
+++ Some-D-coin/src/main.jsx
@@ -14,7 +14,7 @@ import Home from "./components/Home/Home.jsx";
 import Team from "./components/Team/Team.jsx";
 import About from "./components/About/About.jsx";
 import Transaction from "./components/Transaction/Transaction.jsx"
-import Contributors from "./components/Home/Contributors.jsx";
+// import Contributors from "./components/Home/Contributors.jsx";
 import Wallet from "./components/Wallet/Wallet.jsx";
 
 
