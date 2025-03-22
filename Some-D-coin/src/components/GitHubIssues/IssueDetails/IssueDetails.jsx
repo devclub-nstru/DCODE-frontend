b86ee55f908fbf43b4d8951182f992915f6798cb
Aditya kumar <adityakr80510@gmail.com>
2025-03-22 11:49:25 +0530

    update

--- Some-D-coin/src/components/GitHubIssues/IssueDetails/IssueDetails.jsx
+++ Some-D-coin/src/components/GitHubIssues/IssueDetails/IssueDetails.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./IssueDetails.css";
 import { Circle, ArrowLeft } from "lucide-react";
 import { Link, NavLink } from "react-router-dom";
+import axiosInstance from "../../../utils/axiosConfig";
 
-function IssueDetails(props) {
+function IssueDetails() {
+  const [issueDetails, setissueDetails] = useState({});
+  useEffect(() => {
+    (async () => {
+      var { data: axres } = await axiosInstance.get(
+        "/api/marketplace/issues/" +
+          new URL(document.URL).searchParams.get("id")
+      );
+      if (axres.status) {
+        setissueDetails(axres.issue);
+      } else {
+      }
+    })();
+  }, []);
+
   return (
     <>
       <div className="container-issue">
         <NavLink to="/shop">
-          <button className="back-button-issue">
+          <button className="back-button-issue !mt-[2rem]">
             <ArrowLeft size={20} />
             <span>Back</span>
           </button>
@@ -16,23 +31,21 @@ function IssueDetails(props) {
         <div className="issue-card-issue">
           <div className="issue-header-issue">
             <Circle className="status-icon-issue" size={24} />
-            <h2 className="issue-title-issue">Github Issue name</h2>
+            <h2 className="issue-title-issue">{issueDetails.title}</h2>
           </div>
 
-          <div className="skills-section-issue">
+          {/* <div className="skills-section-issue">
             <p>skills - html, css, javascript, etc</p>
-          </div>
+          </div> */}
 
           <div className="description-section-issue">
             <h3 className="description-title-issue">DESCRIPTION</h3>
             <div className="description-box-issue">
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
-                facere voluptate id ea quasi provident. Est quo asperiores
-                officiis deleniti aspernatur tenetur. Cumque in architecto amet
-                ea minus illo possimus voluptatum similique ex fuga, blanditiis
-                suscipit aut fugiat id aliquam fugit laboriosam aspernatur sunt
-                debitis tempora eaque nostrum sapiente qui!
+              <p
+                style={{ whiteSpace: "break-spaces" }}
+                className="max-h-[23rem] overflow-scroll"
+              >
+                {issueDetails.description}
               </p>
             </div>
           </div>
