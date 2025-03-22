b41a32374a9d73c82f2be59197097ee6e321f069
pythonicforge <pythonicforge@gmail.com>
2025-03-22 10:16:58 +0530

    fixed: about page

--- Some-D-coin/src/components/About/About.jsx
+++ Some-D-coin/src/components/About/About.jsx
@@ -8,57 +8,56 @@ function About() {
         <h1>What is Open-Source?</h1>
         <div className="description">
           <p>
-          {"->"} Open-source means that the code is publicly available for anyone to
-          view, use, and modify. Think of it like a recipe that anyone can
-          improve, customize, or share.
-        </p>
-        <p>
-        {"->"} By contributing to open-source projects, developers can fix bugs, add
-          features, and help make the software better for everyone. It’s all
-          about collaboration, learning, and building together!
-        </p>
+            {"->"} Open-source means that the code is publicly available for anyone to
+            view, use, and modify. Think of it like a recipe that anyone can
+            improve, customize, or share.
+          </p>
+          <p>
+            {"->"} By contributing to open-source projects, developers can fix bugs, add
+            features, and help make the software better for everyone. It’s all
+            about collaboration, learning, and building together!
+          </p>
         </div>
-        
       </div>
       <div className="doubt">
         <h1>What is DCODE?</h1>
         <div className="description">
-        <p>
-          {"->"} DCODE is an event where developers come together to work on
-          open-source projects. At DCODE, participants pick problems (called
-          “issues”) from a list of open-source projects they want to work on.
-          They then “buy” these issues using D-Coins, meaning they commit to
-          solving them.
-        </p>
-        <p>
-         {"->"} Once the issues are fixed, they help improve the project, and everyone
-          benefits because the code becomes better. If the participants find an
-          issue or bug while working on the project, they can raise a petition.
-          If the issue is valid, it can be listed on the shop for others to buy
-          in case the person who raised the issue is willing to work on that
-          particular issue.
-        </p>
-        <p>
-         {"->"} It’s like a hackathon, but instead of just building new things from
-          scratch, you’re helping improve existing projects that anyone can use.
-          It’s all about teamwork, learning, and contributing to the wider tech
-          community.
-        </p>
+          <p>
+            {"->"} DCODE is an event where developers come together to work on
+            open-source projects. At DCODE, participants pick problems (called
+            “issues”) from a list of open-source projects they want to work on.
+            They then “buy” these issues using D-Coins, meaning they commit to
+            solving them.
+          </p>
+          <p>
+            {"->"} Once the issues are fixed, they help improve the project, and everyone
+            benefits because the code becomes better. If the participants find an
+            issue or bug while working on the project, they can raise a petition.
+            If the issue is valid, it can be listed on the shop for others to buy
+            in case the person who raised the issue is willing to work on that
+            particular issue.
+          </p>
+          <p>
+            {"->"} It’s like a hackathon, but instead of just building new things from
+            scratch, you’re helping improve existing projects that anyone can use.
+            It’s all about teamwork, learning, and contributing to the wider tech
+            community.
+          </p>
         </div>
       </div>
       <div className="doubt">
         <h1>What are D-Coins?</h1>
         <div className="description">
-        <p>
-        {"->"} D-Coins are like a digital currency that will be used in buying the
-          issues and other exciting rewards up for grabs. It will also serve as
-          a point system for the leaderboard throughout the whole event.
-        </p>
-        <p>
-         {"->"} Every participant will get their own wallets, which will help them
-          store these coins. They can also transfer these coins among themselves
-          if they are willing to do so.
-        </p>
+          <p>
+            {"->"} D-Coins are like a digital currency that will be used in buying the
+            issues and other exciting rewards up for grabs. It will also serve as
+            a point system for the leaderboard throughout the whole event.
+          </p>
+          <p>
+            {"->"} Every participant will get their own wallets, which will help them
+            store these coins. They can also transfer these coins among themselves
+            if they are willing to do so.
+          </p>
         </div>
       </div>
     </div>
